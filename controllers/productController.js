89f7ba533bfc36423cda60f3dcb720c085afb4e8
aimunNaharr <aimunnaharr@gmail.com>
2023-02-08 13:09:50 +0600

    single product api created

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,12 +1,30 @@
-import ProductModel from "../models/product.js"
+import ProductModel from "../models/product.js";
 
-export const createNewProduct=async(req, res)=>{
-     const product=req.body
-     const newProduct=new ProductModel(product)
-    try {
-     const savedProduct =await newProduct.save()
-     res.status(200).json({savedProduct})
-    } catch (error) {
-     res.status(500).json({error})
-    }
+export const createNewProduct = async (req, res) => {
+        const product = req.body;
+        const newProduct = new ProductModel(product);
+        try {
+                const savedProduct = await newProduct.save();
+                res.status(200).json({ savedProduct });
+        } catch (error) {
+                res.status(500).json({ error });
+        }
+};
+
+export const getAllProducts = async (req, res) => {
+        try {
+                const products = await ProductModel.find();
+                res.status(200).send(products);
+        } catch (error) {
+                res.status(500).json({ msg: "Something went wrong" });
+        }
+};
+
+export const getOneProduct=async(req, res)=>{
+        try{
+                const product=await ProductModel.findById(req.params.id)
+                res.status(200).send(product)
+        }catch{
+                res.status(500).json({ msg: "Something went wrong" });
+        }
 }
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { createNewProduct } from "../controllers/productController.js";
+import { createNewProduct, getAllProducts, getOneProduct } from "../controllers/productController.js";
 const router = express.Router();
 
 router.post("/createNew", createNewProduct);
+router.get("/getAll", getAllProducts);
+router.get("/:id", getOneProduct);
 
 export default router;
