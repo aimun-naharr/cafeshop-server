f6b3cf6149eb279fffb5bd87102955e65b82ab56
aimunNaharr <aimunnaharr@gmail.com>
2023-02-27 12:52:31 +0600

    updated

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ import cors from 'cors'
 const app=express()
 
 // middlewares
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-  methods: ['GET','PUT','DELETE', 'POST', 'PATCH'],
-  allowedHeaders: 'Content-Type,Authorization'
-}
-app.use(cors(corsOptions))
+// const corsOptions ={
+//   origin:'*', 
+//   credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200,
+//   methods: ['GET','PUT','DELETE', 'POST', 'PATCH'],
+//   allowedHeaders: 'Content-Type,Authorization'
+// }
+app.use(cors())
 app.use(bodyParser.json({limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true }))
 
