9bc8a5e702ff6dca3d8f44ef31846b40fe87cdbf
aimunNaharr <aimunnaharr@gmail.com>
2023-02-27 18:21:01 +0600

    updated

--- app.js
+++ app.js
@@ -20,7 +20,11 @@ app.use(bodyParser.urlencoded({limit: '30mb', extended: true }))
 
 app.get("/", (req, res) => {
    res.send("Route is working! YaY!");
-   res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
  });
  app.use('/api/products', productRoute)
  app.use('/api/users', userRoute)
